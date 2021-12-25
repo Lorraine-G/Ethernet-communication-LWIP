529ecb5bb847f36afb256066d7a2be18d39c8af2
Lorraine-G <72872447+Lorraine-G@users.noreply.github.com>
2021-12-25 17:01:23 +0800

    Initial Commit

---
+++ Standalone/tcp_echo_client/tcp_echoclient.h
@@ -0,0 +1,50 @@
+/**
+  ******************************************************************************
+  * @file    tcp_echoclient.h
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    31-July-2013 
+  * @brief   Header file for tcp_echoclient.c
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+  
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __TCP_ECHOCLIENT_H__
+#define __TCP_ECHOCLIENT_H__
+
+/* Includes ------------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+#define DEST_IP_ADDR0 192
+#define DEST_IP_ADDR1 168
+#define DEST_IP_ADDR2 99
+#define DEST_IP_ADDR3 207
+    
+#define DEST_PORT 8642
+
+/* Exported functions ------------------------------------------------------- */
+void tcp_echoclient_connect(void);
+
+#endif /* __TCP_ECHOCLIENT_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
